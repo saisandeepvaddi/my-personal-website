049601fcd42c59f58388bd0702a2878416d11fdf
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-07-12 18:13:09 -0400

    hardcode contact links

--- src/components/ContactLinks.js
+++ src/components/ContactLinks.js
@@ -2,25 +2,8 @@ import React from "react";
 import { DiGithubBadge } from "react-icons/di";
 import { FiLinkedin, FiTwitter } from "react-icons/fi";
 import { AiOutlineMail } from "react-icons/ai";
-import { useStaticQuery, graphql } from "gatsby";
 
 function ContactLinks() {
-  const data = useStaticQuery(graphql`
-    query ContactLinksQuery {
-      site {
-        siteMetadata {
-          social {
-            github
-            linkedin
-            twitter
-            email
-          }
-        }
-      }
-    }
-  `);
-  const { github, linkedin, twitter, email } = data.site.siteMetadata.social;
-
   return (
     <div
       className="links"
@@ -31,7 +14,7 @@ function ContactLinks() {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href={`https://github.com/${github}`}
+        href={`https://github.com/saisandeepvaddi`}
         className="social-link"
       >
         <DiGithubBadge size={25} />
@@ -39,7 +22,7 @@ function ContactLinks() {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href={`https://twitter.com/${twitter}`}
+        href={`https://twitter.com/saisandeepvaddi`}
         className="social-link"
       >
         <FiTwitter size={25} />
@@ -47,7 +30,7 @@ function ContactLinks() {
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href={`https://linkedin.com/in/${linkedin}`}
+        href={`https://linkedin.com/in/saisandeepvaddi`}
         className="social-link"
       >
         <FiLinkedin size={25} />
@@ -56,7 +39,7 @@ function ContactLinks() {
         target="_blank"
         rel="noopener noreferrer"
         className="social-link"
-        href={`mailto:${email}`}
+        href={`mailto:saisandeepvaddi@gmail.com`}
       >
         <AiOutlineMail size={25} />
       </a>
