8ab1386ffb90679bc1b43a083d9dabb9e7e22610
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-08-12 15:16:24 -0400

    add ant-table-extensions post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,8 +5,12 @@ import Bio from "../components/bio";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import { rhythm, scale } from "../utils/typography";
+// import mediumZoom from "medium-zoom";
 
 class BlogPostTemplate extends React.Component {
+  // componentDidMount() {
+  //   mediumZoom(document.querySelectorAll("img"));
+  // }
   render() {
     const post = this.props.data.markdownRemark;
     const siteTitle = this.props.data.site.siteMetadata.title;
