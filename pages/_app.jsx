fb40296dcfc34e38bc08ceae433428f794108a91
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-09-12 16:40:34 -0400

    add mdx

---
+++ pages/_app.jsx
@@ -0,0 +1,4 @@
+import "../styles/prism.scss";
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -5,14 +5,17 @@ import { getFrontMatter } from "../../utils/mdxUtils";
 import renderToString from "next-mdx-remote/render-to-string";
 import hydrate from "next-mdx-remote/hydrate";
 import Layout from "../../components/layout";
+import "prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard";
 
 const components = {};
 function Post({ source, frontMatter }) {
   const content = hydrate(source, components);
   return (
     <Layout>
-      <h1>{frontMatter.title}</h1>
-      {content}
+      <article>
+        <h1>{frontMatter.title}</h1>
+        {content}
+      </article>
     </Layout>
   );
 }
@@ -25,9 +28,20 @@ export async function getStaticProps({ params }) {
   const post = blogPostFiles.find((postMeta) => postMeta.slug === slug);
 
   const fileData = readFileSync(post.path);
-  const { content, data } = getFrontMatter(fileData);
-  const source = await renderToString(content, { components, scope: data });
-  return { props: { frontMatter: data, slug, source } };
+  const { content, data: frontMatter } = getFrontMatter(fileData);
+  const source = await renderToString(content, {
+    components,
+    mdxOptions: {
+      remarkPlugins: [
+        require("remark-autolink-headings"),
+        require("remark-slug"),
+        require("remark-code-titles"),
+      ],
+      rehypePlugins: [require("mdx-prism")],
+    },
+    scope: frontMatter,
+  });
+  return { props: { frontMatter, slug, source } };
 }
 
 export async function getStaticPaths() {
